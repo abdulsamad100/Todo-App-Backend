5b719e96f0174502b0e57db142e05e27db42d6f9
Abdul Samad <samadjs100@gmail.com>
2025-01-12 13:33:52 +0500

    hope

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv")
+
+dotenv.config()
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+if(process.env.NODE_ENV!=="production"){
+    app.listen(3000, () => console.log("Server ready on port 3000."));
+}
 
 module.exports = app;
