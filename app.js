19143392922c6644804b99b5862154454bbdc121
Abdul Samad <samadjs100@gmail.com>
2025-01-12 13:39:59 +0500

    hope 2

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-const express = require("express");
-const app = express();
-const cors = require('cors')
-const mongoose = require('mongoose');
-const { authroutes } = require("./routes/auth-routes");
-const authMiddleware = require("./middleware/Auth-Middleware");
-const { todoroutes } = require("./routes/todo-routes");
-const PORT = process.env.PORT;
-const MONGOURL = process.env.MONGO_URL
+// const express = require("express");
+// const app = express();
+// const cors = require('cors')
+// const mongoose = require('mongoose');
+// const { authroutes } = require("./routes/auth-routes");
+// const authMiddleware = require("./middleware/Auth-Middleware");
+// const { todoroutes } = require("./routes/todo-routes");
+// const PORT = process.env.PORT;
+// const MONGOURL = process.env.MONGO_URL
 
-app.use(express.json());
-app.use(cors())
+// app.use(express.json());
+// app.use(cors())
 
-mongoose.connect(MONGOURL).then(() => {
-  console.log("MONGO_DB Connected");
-}).catch((e) => {
-  console.log(e).message;
-})
+// mongoose.connect(MONGOURL).then(() => {
+//   console.log("MONGO_DB Connected");
+// }).catch((e) => {
+//   console.log(e).message;
+// })
 
 
-app.use(express.json());
+// app.use(express.json());
 
-app.get('/',(req,res)=>{
-  console.log("Server Running>>>>>>>>>");
+// app.get('/',(req,res)=>{
+//   console.log("Server Running>>>>>>>>>");
   
-})
-app.use('/auth', authroutes)
+// })
+// app.use('/auth', authroutes)
 
-app.use('/api', authMiddleware, todoroutes)
+// app.use('/api', authMiddleware, todoroutes)
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running at http://localhost:${PORT}`);
+// });
--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require("express");
-const app = express();
 const dotenv = require("dotenv")
+const app = express();
+const cors = require('cors')
+const mongoose = require('mongoose');
+const { authroutes } = require("./routes/auth-routes");
+const authMiddleware = require("./middleware/Auth-Middleware");
+const { todoroutes } = require("./routes/todo-routes");
+const PORT = process.env.PORT;
+const MONGOURL = process.env.MONGO_URL
+
+app.use(express.json());
 
 dotenv.config()
+app.use(express.json());
+app.use(cors())
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+mongoose.connect(MONGOURL).then(() => {
+    console.log("MONGO_DB Connected");
+}).catch((e) => {
+    console.log(e).message;
+})
+
+
+
+app.get('/',(req,res)=>{
+    console.log("Server Running>>>>>>>>>");
+    res.send("Express on Vercel")
+})
+app.use('/auth', authroutes)
+
+app.use('/api', todoroutes)
+
+
 
 if(process.env.NODE_ENV!=="production"){
     app.listen(3000, () => console.log("Server ready on port 3000."));
